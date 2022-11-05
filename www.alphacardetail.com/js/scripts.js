778f9eb0b0a0a1a6ad5043cd693de7a2bccd20a9
Mehmed Selimovic <mehmed.selimovic.ets@gmail.com>
2022-11-05 22:33:05 +0100

    Cookies notification

--- www.alphacardetail.com/js/scripts.js
+++ www.alphacardetail.com/js/scripts.js
@@ -1,5 +1,21 @@
 // Scripts
 //
+// cookies
+
+const cookieBox = document.querySelector(".wrapper"),
+  acceptBtn = cookieBox.querySelector("button");
+acceptBtn.onclick = () => {
+  // 1 month expire
+  document.cookie = "CookieBy=SugarHill; max-age=" + 60 * 60 * 24 * 30;
+  if (document.cookie) {
+    cookieBox.classList.add("hide");
+  } else {
+    alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
+  }
+};
+let checkCookie = document.cookie.indexOf("CookieBy=SugarHill");
+//if set hide box
+checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
 
 window.addEventListener("DOMContentLoaded", (event) => {
   // Navbar shrink function
